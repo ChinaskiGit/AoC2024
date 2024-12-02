259786643e2021e8aff8cfb70b772511454aa06c
DESKTOP-FB2M880\Pablo <pabloi.blesa@gmail.com>
2024-12-02 20:00:30 +0100

    Agregar archivos de proyecto.

---
+++ Dia1/ProblemaDia1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2024.Dia1
+{
+    public static class ProblemaDia1
+    {
+        public static void ResolverParte1(string input)
+        {
+            string inputStr = File.ReadAllText(input);
+            string[] lines = inputStr.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            List<int> startNumbers = [];
+            List<int> endNumbers = [];
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length >= 2)
+                {
+                    startNumbers.Add(int.Parse(parts[0]));
+                    endNumbers.Add(int.Parse(parts[1]));
+                }
+            }
+            startNumbers.Sort();
+            endNumbers.Sort();
+            int totalDistance = startNumbers.Zip(endNumbers, (start, end) => Math.Abs(start - end)).Sum();
+
+            Console.WriteLine(totalDistance);
+        }
+        public static void ResolverParte2(string input)
+        {
+            string inputStr = File.ReadAllText(input);
+            string[] lines = inputStr.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            List<int> startNumbers = [];
+            List<int> endNumbers = [];
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length >= 2)
+                {
+                    startNumbers.Add(int.Parse(parts[0]));
+                    endNumbers.Add(int.Parse(parts[1]));
+                }
+            }
+            var endNumberCounts = endNumbers.GroupBy(x => x).ToDictionary(group => group.Key, group => group.Count());
+            int totalSum = startNumbers.Sum(start => start * (endNumberCounts.TryGetValue(start, out int value) ? value : 0));
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System;
+using Aoc2024.Dia1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProblemaDia1.ResolverParte1("input/input1.txt");
+        ProblemaDia1.ResolverParte2("input/input1.txt");
+        //TODO ProblemaDia2.Resolver("input/input2.txt");
+    }
+}
